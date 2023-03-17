2a00cbfa7864ac8a1dc9880b2182df6a4282b58c
pranjal96-12 <118056171+pranjal96-12@users.noreply.github.com>
2023-03-17 15:33:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import CheckIt from "./Component/CheckIt";
 import Header from "./Component/Header";
